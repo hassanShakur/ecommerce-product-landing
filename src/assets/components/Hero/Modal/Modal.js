c4bc604adc8b7bd2743ae6805e3c4b293fbc34d9
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-11 11:36:07 +0300

    modal complete

--- src/assets/components/Hero/Modal/Modal.js
+++ src/assets/components/Hero/Modal/Modal.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from '../Hero';
+import close from '../../../images/icon-close.svg';
+
 
 const Modal = () => {
   return (
     <div className='modal open-modal'>
       <div>
+        <img className='close' src={close} alt="" />
         <Hero />
       </div>
     </div>
